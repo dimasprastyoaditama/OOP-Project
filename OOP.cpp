e76209e2e0b4f16c3cbc7e3dd71e49ba5d6e94c0
dimasprastyoaditama <prastyod647@gmail.com>
2025-04-29 18:54:09 +0700

    Membuat Object Mahasiswa

--- OOP.cpp
+++ OOP.cpp
@@ -8,6 +8,14 @@ public:
     float nilai;
 
 void printData ()
+{
+    cout << "NIM:" << nim << endl;
+    cout << "Nama:" << nama << endl;
+    cout << "Nilai:" << nilai << endl;
+    }
+};
+
+
 
 
 
