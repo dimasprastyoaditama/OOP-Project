488e83dc0fbd4b7bd641ccacb21adc99a48d727f
dimasprastyoaditama <prastyod647@gmail.com>
2025-04-29 18:46:24 +0700

    Membuat akses modifier public

--- OOP.cpp
+++ OOP.cpp
@@ -3,30 +3,25 @@ using namespace std;
 
 class mahasiswa {
 public:
-   int nim;
-   string nama;
-   float nilai;
+    int nim;
+    string nama;
+    float nilai;
 
-void printData() {
-    cout << "nim" << nim << endl;
-    cout << "nama" << nama << endl;
-    cout << "nilai" << nilai << endl;
-  }
-};
 
-int main() {
-    nim = 20240140114;
-    nama = 'Dimas';
-    nilai = 87,6;
+void printData () {
+    cout << "NIM:" << nim << endl;
+    cout << "Nama:" << nama << endl;
+    cout << "Nilai:" << nilai << endl;
+    }
 };
 
 int main () {
     mahasiswa mhs;
-    mhs.nim = 20240140114;
-    mhs.nama = "Dimas";
-    mhs.nilai = 87,6;
+    mhs.nim = 119;
+    mhs.nama = "Naufal";
+    mhs.nilai = 80.5;
 
+    mhs.printData();
 }
-
 
 
